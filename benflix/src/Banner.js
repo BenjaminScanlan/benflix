bfb6a6ea27338d434abf2c7bea24d1f80a358392
BenjaminScanlan <90295672+BenjaminScanlan@users.noreply.github.com>
2022-10-19 06:52:45 -0700

    build the row component

--- benflix/src/Banner.js
+++ benflix/src/Banner.js
@@ -31,16 +31,16 @@ fetchData();
         <header className='banner'
             style={{
                 backgroundSize: 'cover',
-                backgroundImage: `url(data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxISEhUSEhIVFRUVFRUVFRcXFxcXFxUVFRUWFhUXFxcYHSggGBolHRUVITEhJSkrLi4uFx8zODMtNygtLisBCgoKBQUFDgUFDisZExkrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrK//AABEIALcBEwMBIgACEQEDEQH/xAAXAAEBAQEAAAAAAAAAAAAAAAAAAQIH/8QAFxABAQEBAAAAAAAAAAAAAAAAABEBgf/EABQBAQAAAAAAAAAAAAAAAAAAAAD/xAAUEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBAAIRAxEAPwDjKAAUAUABUAUQAVFAWgAIAuKgAIoIoAFEBRAFwAAABUNA1nVQDdFoDIAAAAACooAAKhoCiKCCgGAAqFAAADSoChgAABoJQaShuAUqaYC8VKAyIAogAACqgCiAKAAqAKhQAAAAAAFQSgoYACALSoA1glABFBRAGQABAFVMAUEBQAUQwFAAEAFQBaJQFEAVNDQDUAKCApQBRAFEAUSgAgABoCoAoAAALRAFABUAAAAKAAAIoCIoAAAAAUQFogCgAgAAAAAAAKIoAAFAAEUA0AAAFQBU0ABABUAUABNVAAAUSAAIAqAKAAAAAAACoACoAqACgAAAAgAAAAKlAAAAAAKAIqAAAKgCiKAAAIoAAAICqgCgAagAqACaKgBgAoigigAABQ6AgAAAIoAAAAAAAqAAAAqAKCAoAAIAAACAqoAoAAAIFAQAFQUBFAAAQAFAAAABQRUUBFAEABUAAAQVAFQBQAAQAAAABUUAAEAABQMAAAAAAAAAAAAAARQEAAAAAAAABAUQBVQBUACgAAAogAqAKAAAAAAUAAAAAEAAQBQAAAAAf//Z)`,
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
                 backgroundPosition: 'center center',
             }}>
             <div className="banner_contents">
-                <h1 className="banner_title">Movie Name</h1>
+                <h1 className="banner_title">{movie?.name || movie?.original_name}</h1>
                 <div className="banner_buttons">
                     <button className='banner_button'>Play</button>
                     <button className='banner_button'>My List</button>
                 </div>
-                <h1 className="banner_description">{truncate(`This is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test description`, 150)}</h1>
+                <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
             </div>
 
             <div className="banner--fadeBottom" />
