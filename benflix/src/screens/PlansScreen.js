65966007c7bd29ee4ebc2aa2b06e2a4553083973
BenjaminScanlan <90295672+BenjaminScanlan@users.noreply.github.com>
2023-06-17 17:21:21 -0700

    latest commit

--- benflix/src/screens/PlansScreen.js
+++ benflix/src/screens/PlansScreen.js
@@ -8,6 +8,25 @@ import { selectUser } from "../features/userSlice";
 function PlansScreen() {
   const [products, setProducts] = useState([]);
   const user = useSelector(selectUser);
+  const [subscription, setSubscription] = useState(null);
+
+  useEffect(() => {
+    db.collection("customers")
+      .doc(user.uid)
+      .collection("subscriptions")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach(async (subscription) => {
+          setSubscription({
+            role: subscription.data().role,
+            current_period_end: subscription.data().current_period_end.seconds,
+            current_period_start:
+              subscription.data().current_period_start.seconds,
+          });
+        });
+      });
+  }, [user.uid]);
+
   useEffect(() => {
     db.collection("products")
       .where("active", "==", true)
@@ -29,7 +48,7 @@ function PlansScreen() {
   }, []);
 
   console.log(products);
-
+  console.log(subscription);
   const loadCheckout = async (priceId) => {
     const docRef = await db
       .collection("customers")
@@ -62,17 +81,39 @@ function PlansScreen() {
 
   return (
     <div className="planScreen">
+      <br />
+      {subscription && (
+        <p>
+          Renewal date:{" "}
+          {new Date(
+            subscription?.current_period_end * 1000
+          ).toLocaleDateString()}
+        </p>
+      )}
       {Object.entries(products).map(([productId, productData]) => {
         //TODO: add some logic to check if the user subscription is acitve...
+        const isCurrentPackage = productData.name
+          ?.toLowerCase()
+          .includes(subscription?.role);
+
         return (
-          <div className="planScreen__plan">
+          <div
+            key={productId}
+            className={`${
+              isCurrentPackage && "plansScreen__plan--disabled"
+            } planScreen__plan`}
+          >
             <div className="planScreen__info">
               <h5>{productData.name}</h5>
               <h6>{productData.description}</h6>
             </div>
 
-            <button onClick={() => loadCheckout(productData.prices.priceId)}>
-              Subscribe
+            <button
+              onClick={() =>
+                !isCurrentPackage && loadCheckout(productData.prices.priceId)
+              }
+            >
+              {isCurrentPackage ? "Current Package" : "Subscribe"}
             </button>
           </div>
         );
